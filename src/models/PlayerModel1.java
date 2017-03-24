0ada7b3d837e94ff7b54eb8e120cea6d45fb4a07
DESKTOP-HAOIUSB\TrKaJv <nguyenthetrinh1996@gmail.com>
2017-03-25 00:41:13 +0700

    no message

--- src/models/PlayerModel1.java
+++ src/models/PlayerModel1.java
@@ -20,48 +20,6 @@ public class PlayerModel1 extends PlayerModel {
         angle = 0;
     }
 
-    //    // Kích cỡ mặc định của người chơi
-//    public static final int DEFAULT_WIDTH = 70;
-//    public static final int DEFAULT_HEGHT = 100;
-//    public static final float ANGLE_CHANGE = 1f;
-//    // Speed mặc địch
-//    private final float SPEED = 3.5F;
-//    // Speed để có thể thay đổi từ bên ngoài
-//    private final float speed;
-//
-//    // Move
-//    private MoveBehavior moveBehavior;
-//    // shoot
-//    private ShootBehavior shootBehavior;
-//    // Nhận vào các phím
-//    private BitSet bitSet;
-//
-//    public BitSet getBitSet() {
-//        return bitSet;
-//    }
-//    // Đạn để shoot
-//
-//    //góc bắn
-//    private float angle;
-//    private static boolean canMove = true;
-//
-//    private int TIME_DELAY_SHOOT = 500;   // Khoản cách của mỗi viên đạn (về time)
-//    private int timeCount = TIME_DELAY_SHOOT;  // thời gian đã trôi qua kể từ khi 1 viên dc bắn ra
-//    private Vector<GameController> bullet;  // Vector gameObject chung để add đạn
-//
-//    public PlayerModel1(int x, int y, int width, int height, BitSet bitSet, Vector<GameController> bullet) {
-//        super(x, y, width, height);
-//        this.bitSet = bitSet;
-//        this.bullet = bullet;
-//        this.speed = SPEED;
-//        this.hp = 100;
-//        angle = 0;
-//    }
-//
-//    public float getAngle() {
-//        return angle;
-//    }
-//
     // Xác định move theo hướng nào
     @Override
     public void move() {
@@ -73,36 +31,6 @@ public class PlayerModel1 extends PlayerModel {
         }
     }
 
-    //
-//    public void moveUp() {
-//        if (y - speed > 0) {
-//            y -= speed;
-//        }
-//    }
-//
-//    public void moveDown() {
-//        if (y + speed < Game.FRAME_HEIGHT - this.getHeight()) {
-//            y += speed;
-//        }
-//    }
-//
-//    public void moveLeft() {
-//        if (x - speed > 0) {
-//            x -= speed;
-//        }
-//    }
-//
-//    public void moveRight() {
-//        if (x + speed < Game.FRAME_WIDTH - this.getWidth()) {
-//            x += speed;
-//        }
-//    }
-//
-//    @Override
-//    public void smartMove() {
-//
-//    }
-//
     @Override
     public void run() {
         super.run();
@@ -116,18 +44,16 @@ public class PlayerModel1 extends PlayerModel {
 
         // set shoot (Nhận xem move bên nào)
         shoot();
+        if (timeCount < TIME_DELAY_SHOOT) {
+            timeCount += Game.GAME_LOOP_TIME;
+        }
+        timeEncreaseBullet +=Game.GAME_LOOP_TIME;
+        encreaseBullet();
+
         if (shootBehavior != null) {
             shootBehavior.shoot(this);
         }
     }
-//
-//    public MoveBehavior getMoveBehavior() {
-//        return moveBehavior;
-//    }
-//
-//    public void setMoveBehavior(MoveBehavior moveBehavior) {
-//        this.moveBehavior = moveBehavior;
-//    }
 
     // Set khi bắn
     public void shoot() {
@@ -143,9 +69,6 @@ public class PlayerModel1 extends PlayerModel {
             }
         }
 
-        if (timeCount < TIME_DELAY_SHOOT) {
-            timeCount += Game.GAME_LOOP_TIME;
-        }
         if (bitSet.get(KeyEvent.VK_SPACE)) {
             shootNormal();
         }
@@ -153,10 +76,11 @@ public class PlayerModel1 extends PlayerModel {
 
     public void shootNormal() {
         // Tăng time kể từ lần bắn trước
-
-        if (timeCount >= TIME_DELAY_SHOOT) {      // Đủ time bắn
+        if (timeCount >= TIME_DELAY_SHOOT && numBullet>0) {      // Đủ time bắn
             // set lại
             timeCount = 0;
+            timeEncreaseBullet = 0;
+            numBullet--;
             // Tăng lượng đạn đã bắn
             // Bắn
             BulletController1 bulletController1 = new BulletController1((int) this.getX() + DEFAULT_WIDTH + 5, this.getMidY(), angle);
--- src/views/PlayerInforView.java
+++ src/views/PlayerInforView.java
@@ -17,26 +17,30 @@ public class PlayerInforView extends GameView {
     @Override
     public void draw(Graphics2D graphics, GameModel gameModel) {
         if(gameModel instanceof PlayerInforModel){
+            // set font stroke
             BasicStroke basicStroke = new BasicStroke(1,BasicStroke.CAP_SQUARE, BasicStroke.JOIN_ROUND);
             (graphics).setStroke(basicStroke);
             graphics.setFont(new java.awt.Font("Britannic",-1, 12));
 
-
+            // Border
             graphics.setColor(new Color(0, 255, 8));
             graphics.drawRect((int) gameModel.getX(), (int)gameModel.getY(), gameModel.getWidth(),  gameModel.getHeight());
 
+            // HP
             graphics.setColor(new Color(255, 18, 0));
             graphics.drawString("HP", gameModel.getX()+20, gameModel.getY()+20);
             graphics.fillRect( (int)gameModel.getX()+50,(int) gameModel.getY()+10, ((PlayerInforModel)gameModel).getHPLeght(), 10);
             graphics.drawRect((int) gameModel.getX()+50,(int) gameModel.getY()+10, PlayerInforModel.DEFAULT_WIDTH_HP, 10);
 
+            // MANA
             graphics.setColor(new Color(9, 19, 255));
             graphics.drawString("MANA", gameModel.getX()+3, gameModel.getY()+40);
             graphics.fillRect( (int)gameModel.getX()+50, (int)gameModel.getY()+30, ((PlayerInforModel)gameModel).getTimeCanShoot(), 10);
             graphics.drawRect( (int)gameModel.getX()+50, (int)gameModel.getY()+30, PlayerInforModel.DEFAULT_WIDTH_HP, 10);
 
-            graphics.setColor(new Color(0, 0, 0));
-            graphics.drawString(((PlayerInforModel) gameModel).getPlayerModel().getHp()+"", gameModel.getX()+60, gameModel.getY()+20);
+            // NUM_BULLET
+            graphics.setColor(new Color(255, 250, 35));
+            graphics.drawString("NUM BUL", gameModel.getX(), gameModel.getY()+60);
 //            graphics.drawString(((PlayerInforModel) gameModel).getPlayerModel().getArmor()+"", gameModel.getX()+60, gameModel.getY()+40);
         }
     }
