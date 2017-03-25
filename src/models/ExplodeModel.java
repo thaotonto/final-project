8da0a740da513f59373a6a07f463baebc33b4ea1
DESKTOP-LPSQP1J\AnhLe <anhltse04277@fpt.edu.vn>
2017-03-25 09:44:45 +0700

    day la animation no ga

--- src/models/ExplodeModel.java
+++ src/models/ExplodeModel.java
@@ -4,15 +4,9 @@ package models;
  * Created by TrKaJv on 25-Mar-17.
  */
 public class ExplodeModel extends  GameModel {
-    boolean checkExployde = true;
 
-    public boolean isCheckExployde() {
-        return checkExployde;
-    }
 
-    public void setCheckExployde(boolean checkExployde) {
-        this.checkExployde = checkExployde;
-    }
+
     public static final int  WIDTH_EPLOYDE = 60;
     public static final int  HEIGHT_EPLOYDE = 60;
     public ExplodeModel(int x, int y) {
