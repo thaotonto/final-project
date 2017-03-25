5cf77c7c1dd4f133c8f7797cf617f5bdefa8d129
DESKTOP-HAOIUSB\TrKaJv <nguyenthetrinh1996@gmail.com>
2017-03-25 08:42:32 +0700

    no message

---
+++ src/controllers/ExplodeCotroller.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import models.GameModel;
+import views.GameView;
+
+/**
+ * Created by TrKaJv on 25-Mar-17.
+ */
+public class ExplodeCotroller extends GameController {
+    public ExplodeCotroller(GameModel model, GameView view) {
+        super(model, view);
+    }
+}
---
+++ src/views/ExplodeView.java
@@ -0,0 +1,12 @@
+package views;
+
+import java.awt.*;
+
+/**
+ * Created by TrKaJv on 25-Mar-17.
+ */
+public class ExplodeView extends GameView {
+    public ExplodeView(Image image) {
+        super(image);
+    }
+}
